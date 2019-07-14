cba30986eae401295dec5d57cd1ecbb4fe7972ad
Carlos manuel De Jesus Puentes valdes <valdes@localhost.localdomain>
2019-07-14 02:48:08 -0300

    Source code

---
+++ tests/partials/testpartial.lua
@@ -0,0 +1,11 @@
+local testpartial = 
+    {'div', class='test', {
+        {'h1', 'Title on partial'},
+        {'h2', 'Subtitle on partial'},
+        {'p', {
+            'Sample text on partial',
+            {'span', 'span on partial'}
+        }}
+    }}
+
+return testpartial
